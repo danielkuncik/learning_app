e64dc621c21d6a474dbd8a916ded538bdfd72a4b
Daniel Kuncik <dkuncik@gmail.com>
2016-09-03 17:49:49 -0400

    Add styling and user controller

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,4 +9,15 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+  
+    # returns the full title on a per-page basis
+  def full_title(page_title = '')
+    base_title = "Learning App"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
+  
 end
