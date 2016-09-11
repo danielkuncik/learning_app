53ead477e4a7065d4bf71d426e26567a196d7f61
Daniel Kuncik <dkuncik@gmail.com>
2016-09-10 21:04:09 -0400

    User Creation Page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,27 @@
 class UsersController < ApplicationController
-  def new
+  
+  def show #renders profile page
+    @user = User.find(params[:id])
   end
+  
+  def new  # redners new user page
+    @user = User.new
+  end
+  
+  def create
+    flash[:success] = "User Creation Successful"
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :section, :password, :password_confirmation)
+  end
+  
 end
--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -9,7 +9,7 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", about_path, count: 2
     assert_select "a[href=?]", signup_path, count: 1
     get signup_path
-    assert_select "title", full_title("Sign up")
+    assert_select "title", full_title("Create User")
   end
 
 end
